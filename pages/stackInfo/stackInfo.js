41219555ffbe7d17b1fd02d47d49e62d8b488b39
Xuguangxing <xugx29@163.com>
2018-04-20 20:38:09 +0800

    fix

--- pages/stackInfo/stackInfo.js
+++ pages/stackInfo/stackInfo.js
@@ -87,5 +87,40 @@ Page({
         page: _this.data.page + 1
       })
     })
+  },
+  onShow(){
+    wx.getSetting({
+      success: result => {
+        if (!result.authSetting['scope.userInfo']) {
+          wx.showModal({
+            title: '用户未授权',
+            // content: '如需正常使用阅读记录功能，请按确定并在授权管理中选中“用户信息”，然后点按确定。最后再重新进入小程序即可正常使用。',
+            content: '如需正常使用甜悦读各项功能，请按确定并在授权管理中选中“用户信息”',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+                wx.openSetting({
+                  success: setting => {
+                    console.log(setting)
+                    if (setting.authSetting['scope.userInfo'] == true) {
+                      wx.getUserInfo({
+                        success: userInfo => {
+                          wx.setStorageSync('userInfo', userInfo.userInfo);
+                          wx.redirectTo({
+                            url: '../index/index',
+                          })
+                        }
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        }
+      }
+    })
   }
+  
 })
