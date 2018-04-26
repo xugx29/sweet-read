758fda9bd9a96ea7e1181ead16994f8c6b2d218a
Xuguangxing <xugx29@163.com>
2018-04-26 09:11:32 +0800

    fix

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -1,5 +1,6 @@
 var app = getApp();
 const utils = require('../../utils/util.js');
+var md5 = require('../../utils/js-md5.js')
 Page({
   data:{
     payLevel : []
@@ -13,15 +14,46 @@ Page({
     })
   },
   pay (event) {
+    var _this = this;
     var money = parseInt(event.currentTarget.dataset.money);
     var dou = parseInt(event.currentTarget.dataset.dou);
     var gift = parseInt(event.currentTarget.dataset.gift)
     var userId = parseInt(wx.getStorageSync('userId'));
     var openId = wx.getStorageSync('openId');
     utils.utilRequest('/mpApi/pay', { userId: userId, openId: openId, count: dou + gift,amount:money }, 'get', function (data) {
-
+      var packageStr = ''
+      for(var n in data.data) {
+        packageStr = n + '=' + data.data[n]
+      }
+      var randomStr = _this.randomString();
+      var times = new Date().getTime();
+      var signStr = 'appId=wx1f25115057119243&nonceStr=' + randomStr + '&package=' + packageStr + '&signType=MD5&timeStamp=' + times + '&key=shanghdingtian201811111111111111'
+      console.log(signStr,'|',md5(signStr))
+      var sign = md5(signStr);
+      wx.requestPayment({
+        timeStamp: times.toString(),
+        nonceStr: randomStr,
+        package: packageStr,
+        signType:'MD5',
+        paySign: sign.toString().toUpperCase(),
+        success: function(){
+          wx.redirectTo({
+            url: '../personal/personal',
+          })
+        }
+      })
     })
     console.log(money,dou)
+  },
+  randomString(len) {
+    　　len = len || 32;
+    　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    // 去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
+    　　var maxPos = $chars.length;
+    　　var pwd = '';
+    　　for(var i = 0; i <len; i++) {
+  　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
   },
   onShow(){
     wx.getSetting({
