90f878e80c290810fc85c1d99c92faec6c832c2a
xuguangxing <xugx29@163.com>
2018-11-27 21:42:33 +0800

    change auth way

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 App({
   onLaunch: function () {
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res);
-      }
-    })
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     console.log(res);
+    //   }
+    // })
     // 获取用户信息
     wx.getSetting({
       success: res => {
